0ec7c3594d443c99b15010c0a9e48c7eb4a9284e
udonrm <b201395@hiroshima-u.ac.jp>
2024-01-21 23:45:27 +0900

    first commit

---
+++ C++入門 AtCoder Programming Guide for beginners (APG4b)/EX1.cpp
@@ -0,0 +1,15 @@
+/*
+`#include <bits/stdc++.h>`
+C++の機能を全て読み込むための命令
+
+`using namespace std`
+`std::cout`や`std::endl`で std::を省略するために名前空間をつかう
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cout << "こんにちは" << endl;
+    cout << "AtCoder" << endl;
+}
